a361f9f6eb9450a2f3165514eb25fc18c9d1ded9
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-04-17 17:07:49 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -14,6 +14,13 @@ end
 
 def get_contestant_name(data, occupation)
   # code here
+  data.each do |season_number, hashes|
+      hashes.each do |contestants|
+        if contestants["occupation"] == occupation
+          return contestants["name"]
+        end
+      end
+    end
 end
 
 def count_contestants_by_hometown(data, hometown)
